f00d3693511ae2a04ca61d371f0fd9056ca97126
manoelgadi <manoelgadi@gmail.com>
2021-05-04 09:00:47 +0000

    sharing details sin QR

--- module001/module001.py
+++ module001/module001.py
@@ -74,6 +74,59 @@ def module001_course():
     courses = Course.query.filter_by(user_id=current_user.id)
     return render_template("module001_course.html",module="module001", form=form, rows=courses)
 
+
+from qrcode import QRCode, ERROR_CORRECT_L
+@module001.route('/sharing_details',methods=['GET','POST'])
+@login_required
+def sharing_details():
+    qr = QRCode(version=20, error_correction=ERROR_CORRECT_L)
+
+    if request.args.get('itemtype') == 'course':
+        course = Course.query.get(request.args.get('rowid'))
+        if not course or course.user_id != current_user.id:
+            flash("An error has occurred retrieving details for the activity")
+            return redirect(url_for('library'))
+        qr.add_data("http://attendance.pythonanywhere.com/follow?sharedlink=1&code={}".format(course.code))
+        module,itemtype,item="library","course",course
+#    else:
+#        participation = ParticipationCode.query.get(request.args.get('rowid'))
+#        if not participation or participation.user_id != current_user.id:
+#            flash("An error has occurred retrieving details for the participation")
+#            return redirect(url_for('participation_generate'))
+#        qr.add_data("http://attendance.pythonanywhere.com/participation_redeem?sharedlink=1&code={}".format(participation.code))
+#        module,itemtype,item="participation_gerenate","participation",participation
+    try:
+        qr.make() # Generate the QRCode itself
+        im = qr.make_image()
+        filename = "./static/qrcodes/{}.png".format(item.code)
+        im.save(filename)
+        return render_template('module001_sharing_details.html',module=module, item=item, itemtype=itemtype,filename=filename,baseurl=request.host)
+    except:
+        return render_template('module001_sharing_details.html',module=module, item=item, itemtype=itemtype,base_url=request.host)
+
+
+
 @module001.route('/test')
 def module001_test():
     return 'OK'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
